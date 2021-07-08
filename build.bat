af9af5f8998625ec15437222418536ac7f99fde6
Juan Burgos <juanlb1988@gmail.com>
2021-07-08 18:58:06 -0400

    Simplify build script by removing manifest.txt file that is unused.

--- build.bat
+++ build.bat
@@ -1,7 +1,5 @@
 :: COMPILES THE PROGRAM BY RUNNING THE MAKEFILE UNDER THE "SRC" DIRECTORY
 @ECHO OFF
 CD src
-ECHO Main-Class: Engine.MainClass > manifest.txt
 javac ./Engine/*.java
 jar cvfe game.jar Engine.MainClass Engine/*.class
-DEL manifest.txt
