dd98e0025e7420f9e2ba7d6395375fabe67cbc24
Juan Burgos <juanlb1988@gmail.com>
2021-07-09 00:11:14 -0400

    Switch back to lower-case for built-in method calls.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 :: COMPILES THE PROGRAM UNDER THE "SRC" DIRECTORY
-@ECHO OFF
-CD src
+@echo off
+cd src
 javac .\Engine\*.java
 jar cvfe game.jar Engine.MainClass Engine/*.class
 del .\Engine\*.class
