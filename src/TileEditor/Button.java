16d96907ff67dbee0651c7b4e4c97560061d5fb6
JoseBurgos1993 <burgosjd1993@gmail.com>
2021-07-14 21:29:18 -0400

    Tile editor is able to draw. Doesn't save art. Able to write name, but again, doesn't save. Error in console related to pressing mouse, not releasing.

--- src/TileEditor/Button.java
+++ src/TileEditor/Button.java
@@ -10,14 +10,42 @@ public class Button {
 	public String text;
 	public boolean pressed = false;
 	public String action;
+	public boolean isNameField = false;
+	public boolean isColor = false;
+	public int tileSpot = -1;
+	public int color = 0;
 	
-	public Button(int x, int y, int w, int h, String text, String action) {
+	public Button(int x, int y, int w, int h, String text, String action, boolean isNameField) {
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
 		this.text = text;
 		this.action = action;
+		this.isNameField = isNameField;
+		if(action == "color 0") {
+			color = 0;
+		}
+		else if(action == "color 1") {
+			color = 1;
+		}
+		else if(action == "color 2") {
+			color = 2;
+		}
+		else if(action == "color 3") {
+			color = 3;
+		}
+	}
+	
+	public Button(int x, int y, int w, int h, String text, int tileSpot, boolean isNameField) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.text = text;
+		this.tileSpot = tileSpot;
+		this.isNameField = isNameField;
+		this.isColor = true;
 	}
 	
 	public Button() { // Default Constructor
@@ -29,6 +57,33 @@ public class Button {
 		action = "None";
 	}
 	
+	public void drawTileColors(Graphics2D g2d) {
+		g2d.setColor(Color.black);
+		g2d.drawRect(x, y, w, h);
+		
+		switch(color) {
+			case 0: g2d.setColor(Color.white); break;
+			case 1: g2d.setColor(Color.LIGHT_GRAY); break;
+			case 2: g2d.setColor(Color.DARK_GRAY); break;
+			case 3: g2d.setColor(Color.black); break;
+			case 4: g2d.setColor(Color.green); break;
+		}
+		g2d.fillRect(x+1, y+1, w-1, h-1);
+		
+	}
+	
+	public void drawNameField(Graphics2D g2d) {
+		g2d.setColor(Color.black);
+		g2d.drawRect(x, y, w, h);
+
+		g2d.setColor(Color.LIGHT_GRAY);
+		g2d.fillRect(x+1, y+1, w-2, h-2);
+		
+		g2d.setColor(Color.BLACK);
+		
+		g2d.drawString(text, x+5, y+h/2+3);
+	}
+	
 	public void drawButton(Graphics2D g2d) {
 		g2d.setColor(Color.blue);
 		//g2d.fillRect(x, y, w, h);
@@ -36,7 +91,7 @@ public class Button {
 		int thickness = 4;
 		g2d.fill3DRect(x, y, w, h, !pressed);
 		for (int i = 1; i <= thickness; i++) {
-		      g2d.draw3DRect(x - i, y - i, w + 2 * i - 1, h + 2 * i - 1, true);
+		      g2d.draw3DRect(x - i, y - i, w + 2 * i, h + 2 * i, true);
 		}
 		Font font = new Font("Verdana", Font.BOLD, 12);
 		g2d.setFont(font);
--- src/TileEditor/TilePanel.java
+++ src/TileEditor/TilePanel.java
@@ -23,10 +23,19 @@ public class TilePanel extends JPanel implements Runnable, KeyListener, MouseLis
 	private Thread thread;
 	private long targetTime;
 	private static final int FRAME_RATE = 60;
+	private String state = "MainMenu";
 	
 	// Buttons \\
 	private Button buttonList[] = new Button[3];
 	private Button activeButton = new Button();
+	private boolean nameFieldActive = false;
+	private String nameFieldText = "";
+	
+	// Tiles \\
+	private Button[] tileColors = new Button[64];
+	private Button[] colorSelection = new Button[5];
+	private int pickedTile = -1;
+	private int selectedColor = 0;
 	
 	public TilePanel() {
 		setPreferredSize(new Dimension(800, 600));
@@ -57,18 +66,8 @@ public class TilePanel extends JPanel implements Runnable, KeyListener, MouseLis
 			startTime = System.nanoTime();
 			update();
 			requestRender();
-
-			// For testing \\
-			//frame_count++;
 			elapsed = System.nanoTime() - startTime;
 			wait = targetTime - elapsed / 1000000;
-			
-			//if(frame_count == FRAME_RATE) {
-			//	System.out.println("Time Used = " + elapsed / 1000000 + "/" + targetTime);
-			//	frame_count = 0;
-			//}
-			/////////\\\\\\\\
-			
 			if(wait > 0) {
 				try {
 					Thread.sleep(wait);
@@ -89,13 +88,79 @@ public class TilePanel extends JPanel implements Runnable, KeyListener, MouseLis
 	}
 	
 	private void makeMainMenu() {
-		buttonList[0] = new Button(100,350, 100, 50, "Create", "Create");
-		buttonList[1] = new Button(300,350, 100, 50, "Edit", "Edit");
-		buttonList[2] = new Button(500,350, 100, 50, "Leave", "Leave");
+		state = "MainMenu";
+		buttonList = new Button[3];
+		tileColors = new Button[64];
+		buttonList[0] = new Button(100,350, 100, 50, "Create", "gotoCreate", false);
+		buttonList[1] = new Button(300,350, 100, 50, "Edit", "Edit", false);
+		buttonList[2] = new Button(500,350, 100, 50, "Leave", "Leave", false);
+		nameFieldActive = false;
+	}
+	
+	private void makeCreateScreen() {
+		state = "Create";
+		buttonList = new Button[3];
+		buttonList[0] = new Button(680,20, 100, 50, "Back", "gotoMainMenu", false);
+		buttonList[1] = new Button(680,200,100,50, "Save", "Save", false);
+		buttonList[2] = new Button(10,10,200,50, "", "clickedNameField", true);
+		nameFieldActive = true;
+
+		tileColors = new Button[64];
+
+		for(int i = 0; i < 8; i++) { // Vertical
+			for(int j = 0; j < 8; j++) { // Horizonal
+				tileColors[j+i*8] = new Button(40+42*j, 100+42*i, 40, 40, "", "color", false);
+			}
+		}
+		
+		colorSelection[0] = new Button(600, 300, 40, 40, "", "color 0", false);
+		colorSelection[1] = new Button(600, 350, 40, 40, "", "color 1", false);
+		colorSelection[2] = new Button(600, 400, 40, 40, "", "color 2", false);
+		colorSelection[3] = new Button(600, 450, 40, 40, "", "color 3", false);
 	}
 	
 	private void doButtonAction(String action) {
-		
+		if(action == "Leave") {
+			System.exit(0);
+		}
+		else if(action == "gotoCreate") {
+			makeCreateScreen();
+		}
+		else if(action == "gotoMainMenu") {
+			makeMainMenu();
+		}
+		else if(action == "Save") {
+			System.out.println("Pressed the save button.");
+		}
+		else if(action == "Edit") {
+			System.out.println("Pressed the edit button.");
+		}
+		else if(action == "clickedNameField") {
+			System.out.println("Pressed the name field.");
+			nameFieldActive = true;
+		}
+		else if(action == "color 0") {
+			System.out.println("Pressed the color 0.");
+			selectedColor = 0;
+		}
+		else if(action == "color 1") {
+			System.out.println("Pressed the color 1.");
+			selectedColor = 1;
+		}
+		else if(action == "color 2") {
+			System.out.println("Pressed the color 2.");
+			selectedColor = 2;
+		}
+		else if(action == "color 3") {
+			System.out.println("Pressed the color 3.");
+			selectedColor = 3;
+		} else if(action == "color") {
+			changeColor();
+		}
+	}
+	
+	private void changeColor() {
+		activeButton.color = selectedColor;
 	}
 	
 	private void update() {
@@ -110,10 +175,29 @@ public class TilePanel extends JPanel implements Runnable, KeyListener, MouseLis
 	}
 	
 	private void render(Graphics2D g2d) {
-		
+		g2d.clearRect(0, 0, 800, 600);
 		for(Button b : buttonList) {
-			b.drawButton(g2d);
+			if(b.isNameField) {
+				b.drawNameField(g2d);
+			} else if(b.tileSpot == -1){
+				b.drawButton(g2d);
+			}
 		}
+		if(state == "Create") {
+			g2d.setColor(Color.red);
+			g2d.fillRect(30,90,334,334);
+			for(Button b : tileColors) {
+				b.drawTileColors(g2d);
+			}
+			
+			g2d.setColor(Color.red);
+			g2d.fillRect(590,290,60,220);
+			colorSelection[0].drawTileColors(g2d);
+			colorSelection[1].drawTileColors(g2d);
+			colorSelection[2].drawTileColors(g2d);
+			colorSelection[3].drawTileColors(g2d);
+		}
+		
 	}
 	
 	@Override
@@ -124,13 +208,38 @@ public class TilePanel extends JPanel implements Runnable, KeyListener, MouseLis
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		char key = e.getKeyChar();
+		//String keyLetter = "" + key; // Horrible way to convert a char to a string
+		if(nameFieldActive && nameFieldText.length() < 20) {
+			// if((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z') || (letter >= 0 && letter <= 9))
+			//int letter = Integer.parseInt(keyLetter);
+			
+			
+			if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+				System.out.println("Pressed back space");
+				nameFieldText = nameFieldText.substring(0, nameFieldText.length() - 1);
+			} else{
+				System.out.println(key);
+				nameFieldText += key;
+			}
+			
+			for(Button b : buttonList) {
+				if(b.isNameField) {
+					b.text = nameFieldText;
+				}
+			}
+		} else if(nameFieldActive && nameFieldText.length() == 20 && e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			nameFieldText = nameFieldText.substring(0, nameFieldText.length() - 1);
+			for(Button b : buttonList) {
+				if(b.isNameField) {
+					b.text = nameFieldText;
+				}
+			}
+		}
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -153,14 +262,26 @@ public class TilePanel extends JPanel implements Runnable, KeyListener, MouseLis
 
 	@Override
 	public void mousePressed(java.awt.event.MouseEvent e) {
-		// TODO Auto-generated method stub
 		int x = e.getX();
 		int y = e.getY();
 		for(Button b : buttonList) {
 			if(x > b.x && x < b.x + b.w && y > b.y && y < b.y + b.h) {
-				System.out.println("You pressed the " + b.text + " button!");
 				b.pressed = true;
 				activeButton = b;
+			}
+		}
+		if(state == "Create") {
+			for(Button b : tileColors) {
+				if(x > b.x && x < b.x + b.w && y > b.y && y < b.y + b.h) {
+					b.pressed = true;
+					activeButton = b;
+				}
+			}
+			for(Button b : colorSelection) {
+				if(x > b.x && x < b.x + b.w && y > b.y && y < b.y + b.h) {
+					b.pressed = true;
+					activeButton = b;
+				}
 			}
 		}
 		
@@ -171,13 +292,17 @@ public class TilePanel extends JPanel implements Runnable, KeyListener, MouseLis
 		int x = e.getX();
 		int y = e.getY();
 		activeButton.pressed = false;
+		
 		if(x > activeButton.x && x < activeButton.x + activeButton.w && y > activeButton.y && y < activeButton.y + activeButton.h) {
-			System.out.println("You activated the " + activeButton.text + " button!");
+			System.out.println(activeButton.isColor);
 			doButtonAction(activeButton.action);
+			//if(activeButton.isColor) {
+			//	changeColor();
+			//} else {
+			//	
+			//}
 		} else {
-			System.out.println("You failed to activate the " + activeButton.text + " button.");
+			nameFieldActive = false;
 		}
 	}
-
-
 }
